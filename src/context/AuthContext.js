dd18b4c7eeff33f0c7fcb0c9c2e7c2d04f1b78f9
Mark-Sarmiento <markrafaelsarmiento9@gmail.com>
2023-06-20 19:03:21 +0800

    init

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,7 +1,7 @@
 import { useContext, createContext, useEffect, useState } from 'react';
 import {
   GoogleAuthProvider,
-  signInWithRedirect,
+  signInWithPopup,
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
@@ -14,7 +14,7 @@ export const AuthContextProvider = ({ children }) => {
 
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
-    signInWithRedirect(auth, provider);
+    signInWithPopup(auth, provider);
   };
 
   const logOut = () => {
